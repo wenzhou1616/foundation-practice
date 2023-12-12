077a7f9fbe191480f60ffd11427a83fcb9ae35db
Herry <1833066494@qq.com>
2023-12-12 18:36:58 +0800

    docs：重构java基础部分

--- src/main/java/com/herry/code/Test.java
+++ src/main/java/com/herry/code/Test.java
@@ -1,14 +1,12 @@
 package com.herry.code;
 
-import com.herry.code.dome.MyRunnable;
-import com.herry.code.dome.MyThread;
-
 /**
  * @author herry
  */
 public class Test {
+
     public static void main(String[] args) {
-        System.out.println("aaaa");
+
     }
 }
 
